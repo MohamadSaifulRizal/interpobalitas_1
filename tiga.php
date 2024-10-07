6e524fa3e0a22161d85ca50358d561391ab816d9
MOHAMAD SAIFUL RIZAL <rizalbwi201@gmail.com>
2024-10-07 08:26:06 +0700

    lagi

--- tiga.php
+++ tiga.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://jsonplaceholder.typicode.com/posts/1'; // ID post yang akan dihapus
+$url = 'https://jsonplaceholder.typicode.com/posts/1';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
